0141530c3ba60e8efdc6cd5f7dfae43cabc0a6cf
jerrinfrncs <jerrinfrncs@gmail.com>
2017-02-12 09:32:12 +0530

    Update batterynotif(uname).sh

--- batterynotif(uname).sh
+++ batterynotif(uname).sh
@@ -3,6 +3,8 @@
 POWERSUPPLY="/sys/class/power_supply/ADP1/online" # could be different on your system!
 TOO_LOW=25 # how low is too low?
 NOT_CHARGING="0"
+
+#Change for your distribution
 ICONH="/usr/share/icons/Arc/apps/24@2x/unity-power-panel.png"
 ICONL="/usr/share/icons/Arc/panel/22/gpm-battery-020.svg"
 ICONC="/usr/share/icons/Faba/24x24/panel/gpm-battery-000.svg"
@@ -14,29 +16,31 @@ do
 	STATUS=$(cat $POWERSUPPLY) #charger connected or not
     	battery_level=`upower -i $(upower -e | grep BAT) | grep --color=never -E percentage|xargs|cut -d' ' -f2|sed s/%//`
 
-    	if [ $battery_level -ge 80 -a $STATUS = 1 ]
+    	if [ $battery_level -ge 80 -a $STATUS = 1 ] #Battery Full
     	then
         	/usr/bin/notify-send -i "$ICONH" "Battery Charged" "Battery level is ${battery_level}%!" 
 	paplay /usr/share/sounds/freedesktop/stereo/complete.oga
    
-    	elif [ $battery_level -le 25 -a $STATUS = $NOT_CHARGING ]
+    	elif [ $battery_level -le 25 -a $STATUS = $NOT_CHARGING ]#Battery Low 1
     	then
        		/usr/bin/notify-send -i "$ICONL" "Battery low" "Battery level is ${battery_level}%!" 
 	 paplay /usr/share/sounds/freedesktop/stereo/dialog-warning.oga  
 
-	elif [ $battery_level -le 15 -a $STATUS = $NOT_CHARGING ]
+	elif [ $battery_level -le 15 -a $STATUS = $NOT_CHARGING ]#Battery Low 2
     	then
        		/usr/bin/notify-send -i "$ICONL" "Battery Critical" "Battery level is ${battery_level}%!" 
 	 paplay /usr/share/sounds/freedesktop/stereo/dialog-warning.oga  
 
-	elif [ $battery_level -le 10 -a $STATUS = $NOT_CHARGING ]
+	elif [ $battery_level -le 10 -a $STATUS = $NOT_CHARGING ]#Battery Low 3 && Shutdown Warning
     	then
        		/usr/bin/notify-send -i "$ICONC" "About to shutdown connect charger" "Battery level is ${battery_level}%!" 
 	 paplay /usr/share/sounds/freedesktop/stereo/dialog-warning.oga
-         sleep 30s
-	/usr/bin/notify-send -I "$ICONS" "Too late, Shutting down"
-	sleep 15s
-	 systemctl poweroff 
+         	sleep 30s
+		
+		##Shutting Down
+		/usr/bin/notify-send -I "$ICONS" "Too late, Shutting down"
+		sleep 15s
+	 	systemctl poweroff 
     	fi
    sleep 2m
 done
