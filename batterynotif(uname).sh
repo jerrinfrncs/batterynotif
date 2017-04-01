b5b5674f22b23eaa10ff41eafbb436467c39c568
jerrinfrncs <jerrinfrncs@gmail.com>
2017-04-01 10:50:35 +0530

    Logical error
    Order of conditions changed

--- batterynotif(uname).sh
+++ batterynotif(uname).sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 POWERSUPPLY="/sys/class/power_supply/ADP1/online" # could be different on your system!
 TOO_LOW=25 # how low is too low?
 NOT_CHARGING="0"
-
-#Change for your distribution
 ICONH="/usr/share/icons/Arc/apps/24@2x/unity-power-panel.png"
 ICONL="/usr/share/icons/Arc/panel/22/gpm-battery-020.svg"
 ICONC="/usr/share/icons/Faba/24x24/panel/gpm-battery-000.svg"
@@ -16,33 +14,33 @@ do
 	STATUS=$(cat $POWERSUPPLY) #charger connected or not
     	battery_level=`upower -i $(upower -e | grep BAT) | grep --color=never -E percentage|xargs|cut -d' ' -f2|sed s/%//`
 
-    	if [ $battery_level -ge 80 -a $STATUS = 1 ] #Battery Full
+    	if [ $battery_level -ge 80 -a $STATUS = 1 ] #battery Low 3
     	then
         	/usr/bin/notify-send -i "$ICONH" "Battery Charged" "Battery level is ${battery_level}%!" 
 	paplay /usr/share/sounds/freedesktop/stereo/complete.oga
-   
-    	elif [ $battery_level -le 25 -a -gt 15 -a $STATUS = $NOT_CHARGING ]#Battery Low 1
-    	then
-       		/usr/bin/notify-send -i "$ICONL" "Battery low" "Battery level is ${battery_level}%!" 
-	 paplay /usr/share/sounds/freedesktop/stereo/dialog-warning.oga  #check for your os
 
-	elif [ $battery_level -le 15 -a -gt 10 -a $STATUS = $NOT_CHARGING ]#Battery Low 2
-    	then
-       		/usr/bin/notify-send -i "$ICONL" "Battery Critical" "Battery level is ${battery_level}%!" 
-	 paplay /usr/share/sounds/freedesktop/stereo/dialog-warning.oga  #check for your os
-
-	elif [ $battery_level -le 10 -a $STATUS = $NOT_CHARGING ]#Battery Low 3 && Shutdown Warning
+	elif [ $battery_level -le 10 -a $STATUS = $NOT_CHARGING ] #battery low 2
     	then
        		/usr/bin/notify-send -i "$ICONC" "About to shutdown connect charger" "Battery level is ${battery_level}%!" 
-	 paplay /usr/share/sounds/freedesktop/stereo/dialog-warning.oga #check for your os
+	 	paplay /usr/share/sounds/freedesktop/stereo/dialog-warning.oga
          	sleep 30s
-		
-		##Shutting Down
 		/usr/bin/notify-send -I "$ICONS" "Too late, Shutting down"
 		sleep 15s
 	 	systemctl poweroff 
+
+	elif [ $battery_level -le 15 -a $STATUS = $NOT_CHARGING ] #battery low 1
+    	then
+       		/usr/bin/notify-send -i "$ICONL" "Battery Critical" "Battery level is ${battery_level}%!" 
+	 	paplay /usr/share/sounds/freedesktop/stereo/dialog-warning.oga  
+   
+    	elif [ $battery_level -le 25 -a $STATUS = $NOT_CHARGING ]
+    	then
+       		/usr/bin/notify-send -i "$ICONL" "Battery low" "Battery level is ${battery_level}%!" 
+	 paplay /usr/share/sounds/freedesktop/stereo/dialog-warning.oga  
+
+	
+	
     	fi
    sleep 2m
 done
     
-
